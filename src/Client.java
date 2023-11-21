a939ea7321a45a8ab88211f5e19b84685adc501f
Hasibul Hossain <148477986+Heisenberg293@users.noreply.github.com>
2023-11-21 23:32:17 +0600

    Hashmap added...

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Client {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 8080);
+            System.out.println("Client Connected");
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+
+
+            new Thread(() -> {
+                try {
+                    Scanner in = new Scanner(socket.getInputStream());
+
+                    while (in.hasNext()) {
+                        String receivedMessage = in.nextLine();
+                        System.out.println(receivedMessage);
+
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            while (true) {
+                String message = scanner.nextLine();
+                out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
